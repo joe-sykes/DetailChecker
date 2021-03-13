3372035a253b4f7370193f6d70b4c1fdfec1ea0f
joe-sykes <joe.sykes99@gmail.com>
2021-03-13 13:41:07 +0000

    Add files via upload

--- details_checker.py
+++ details_checker.py
@@ -1,10 +1,17 @@
 #FUNCTION IMPORT LIST
 #   csv is built in
 import csv
+#   datetime is built in
+from datetime import date
+from datetime import datetime
 
 #Define the directory here relative to where this file is stored
 monthlyStatementDirectory = "DETAILS/monthlyStatement.csv"
 paymentDirectory = "DETAILS/database.csv"
+
+#ENTER NAME
+username = str("J. Sykes")
+
 
 with open(paymentDirectory, 'r') as t1, open(monthlyStatementDirectory, 'r') as t2:
     fileone = t1.readlines()
@@ -25,3 +32,13 @@ with open('update.txt', 'w') as outFile:
     outFile.write("\nThese are the entries which were tested:\n \t")
     for line in filetwo:
         outFile.write(line + "\t")
+    
+
+    outFile.write("\n########################################\n\n\n ")
+    today = date.today()
+    theDate = today.strftime("%d %B %Y")
+
+    now = datetime.now()
+    dt_string = str(now.strftime("%H:%M:%S"))
+    metaData = str("\n This report was generated on: "+ theDate +" at "+ dt_string + " by " + username + " with software created by Joe Sykes")
+    outFile.write(metaData)
